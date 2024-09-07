a849d38b752bd3c7db6e36c1445f21ea9baa3ef9
artem <vadis1766@gmail.com>
2024-09-07 19:32:35 +0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CARY/', // Убедитесь, что это соответствует имени вашего репозитория
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/auth': {
+        target: 'https://simi129.github.io/CARY', // Обновите на URL вашего приложения
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/auth/, '/auth')
+      }
+    }
+  },
+  // Удаляем экспериментальную настройку renderBuiltUrl
 })
